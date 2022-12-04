afa47c6463140cc7b2c1edb1f85f298664164d25
ari <arikarim146@gmail.com>
2022-12-04 14:36:40 +0300

    Update tests

--- spec/rabbit_carrots_spec.rb
+++ spec/rabbit_carrots_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RabbitCarrots do
   it "has a version number" do
     expect(RabbitCarrots::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
